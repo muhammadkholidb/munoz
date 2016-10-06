866251276460da580759bd27118c285169facb7f
Eaton Munoz <eaton.munoz@gmail.com>
2016-10-06 12:34:54 +0000

    Add new file Strings.java

---
+++ munoz-common/src/main/java/hu/pe/munoz/common/helper/Strings.java
@@ -0,0 +1,35 @@
+package hu.pe.munoz.common.helper;
+
+import java.io.File;
+
+public class Strings {
+    
+    public static final String EMPTY = "";
+    
+    public static final String FILE_SEPARATOR = File.separator;
+    
+    public static final String NEW_LINE = System.lineSeparator();
+    
+    public static final String SPACE = " ";
+    
+    public static final String TAB = "\t";
+    
+    public static final String SINGLE_QUOTE = "'";
+    
+    public static final String DOUBLE_QUOTE = "\"";
+    
+    public static final String DOT = ".";
+    
+    public static final String COMMA = ",";
+    
+    public static final String COLON = ":";
+    
+    public static final String SEMICOLON = ";";
+    
+    public static final String PLUS = "+";
+    
+    public static final String MINUS = "-";
+    
+    public static final String UNDERSCORE = "_";
+    
+}
