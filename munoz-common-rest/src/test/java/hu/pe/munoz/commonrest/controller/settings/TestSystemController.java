141c9fe1ddb9b8671dc722ec0585ef7611be7364
xbricklayer <xbricklayer@gmail.com>
2016-10-19 02:24:00 +0700

    Fix rest tests

--- munoz-common-rest/src/test/java/hu/pe/munoz/commonrest/controller/settings/TestSystemController.java
+++ munoz-common-rest/src/test/java/hu/pe/munoz/commonrest/controller/settings/TestSystemController.java
@@ -1,54 +1,31 @@
 package hu.pe.munoz.commonrest.controller.settings;
 
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import hu.pe.munoz.commonrest.controller.settings.SystemController;
+import hu.pe.munoz.common.helper.CommonConstants;
+import hu.pe.munoz.commonrest.controller.AbstractSpringIntegrationTest;
+import hu.pe.munoz.commonrest.helper.ResponseWrapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { 
         "classpath:munoz-common-rest-context-test.xml",
         "classpath:munoz-common-data-context-test.xml" })
 @WebAppConfiguration
-public class TestSystemController {
+public class TestSystemController extends AbstractSpringIntegrationTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(TestSystemController.class);
-
-    private static boolean initialized = false;
-    
-    private MockMvc mockMvc;
-
-    @Autowired
-    private SystemController systemController;
-    
-    @Before
-    public void init() {
-        
-        // Initialize test only once in this class.
-        // Annotation @BeforeClass needs static method to run which will require the class properties to be static too, so the @Autowired cannot be done.
-        // Simple solution from here http://stackoverflow.com/questions/32952884/junit-beforeclass-non-static-work-around-for-spring-boot-application#answer-32956539
-        if (!initialized) {            
-            
-            // http://stackoverflow.com/questions/29053974/how-to-i-write-a-unit-test-for-a-spring-boot-controller-endpoint#answer-29054326
-            mockMvc = standaloneSetup(systemController).build();
-            
-            initialized = true;
-        }
-    }
 
     @Test
     public void testGetSystemList() {
@@ -58,9 +35,16 @@ public class TestSystemController {
                     .andExpect(status().isOk())
                     .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                     .andReturn();
-            LOG.debug("Result: " + result.getResponse().getContentAsString());
+            String response = result.getResponse().getContentAsString();
+            LOG.debug("Response: {}", response);
+            ResponseWrapper wrapper = convertJson(response, ResponseWrapper.class);
+            Assert.assertNotEquals(CommonConstants.EMPTY_STRING, response); 
+            Assert.assertNotNull(wrapper);
+            Assert.assertEquals(CommonConstants.SUCCESS, wrapper.getStatus());
+            Assert.assertEquals(CommonConstants.EMPTY_STRING, wrapper.getMessage());
         } catch (Exception e) {
             LOG.error(e.getMessage(), e);
+            Assert.fail();
         }
     }
 
